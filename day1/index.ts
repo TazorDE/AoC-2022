f634f4ac129e5716e8fc6ee0f800b9ac1f9b76ff
Malte Teichert <malte@malteteichert.de>
2022-11-30 21:38:39 +0100

    set up first day

---
+++ day1/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright 2022 - Malte Teichert
+ * Advent of Code Day 1
+ */
+
+// import statements
+import { readFileSync } from "fs";
+
+// read input from input.txt
+const input: string = readFileSync("./input.txt", {
+  encoding: "utf8",
+  flag: "r",
+});
+const lines: string[] = input.split(/\r?\n/);
+
+// Challenge:
