1e481090a863c177653c8747b20cab8256c6fc91
Malte Teichert <malte@malteteichert.de>
2022-12-06 13:40:49 +0100

    solve day6 and add day7 template

--- day6/index.ts
+++ day6/index.ts
@@ -1,6 +1,5 @@
 /**
  * Copyright 2022 - Malte Teichert
- * CRATE MOVER 9000
  * Advent of Code Day 6
  */
 
@@ -8,9 +7,45 @@
 import { readFileSync } from "fs";
 
 // read input from input.txt
-const input: string = readFileSync("./testinput.txt", {
+const input: string = readFileSync("./input.txt", {
   encoding: "utf8",
   flag: "r",
 });
 const lines: string[] = input.split(/\r?\n/);
 
+
+let firstMarker: number = findFirstMarker(lines[0]);
+let messageMarker: number = findMessageMarker(lines[0]);
+if (firstMarker !== -1) {
+  console.log('firstMarker:', firstMarker);
+};
+if (messageMarker !== -1) {
+  console.log('messageMarker:', messageMarker);
+};
+
+function findFirstMarker(buffer: string | unknown[]) {
+  // Loop through the buffer, starting from the fourth character
+  for (let i = 3; i < buffer.length; i++) {
+    // Check if the previous four characters are all different
+    if (new Set(buffer.slice(i - 3, i + 1)).size === 4) {
+      // Return the current index, which is the number of characters
+      // from the beginning of the buffer to the end of the first marker
+      return i + 1;
+    }
+  }
+  // If no marker is found, return -1
+  return -1;
+}
+function findMessageMarker(buffer: string | unknown[]) {
+  // Loop through the buffer, starting from the 14th character
+  for (let i = 13; i < buffer.length; i++) {
+    // Check if the previous 14 characters are all different
+    if (new Set(buffer.slice(i - 13, i + 1)).size === 14) {
+      // Return the current index, which is the number of characters
+      // from the beginning of the buffer to the end of the first marker
+      return i + 1;
+    }
+  }
+  // If no marker is found, return -1
+  return -1;
+}
