76c2fba41d0e0ba6d3133edfda57aa6ddde63e93
Malte Teichert <malte@malteteichert.de>
2022-12-09 17:12:28 +0100

    add day9 template

---
+++ day9/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2022 - Malte Teichert
+ * Advent of Code Day 9
+ */
+
+// import statements
+import { readFileSync } from "fs";
+
+// read input from input.txt
+const input: string = readFileSync("./input.txt", {
+  encoding: "utf8",
+  flag: "r",
+});
+const lines: string[] = input.split(/\r?\n/);
+
